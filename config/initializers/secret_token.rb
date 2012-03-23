06a6f2b77d9d0f519d680b6a8102dc2fa3809526
Constantine <paranoid@DXM.localdomain>
2012-03-24 02:29:33 +0400

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Trolley::Application.config.secret_token = '48d76f20c1d25469de29eea3c6bb6914a5b2be27265272af89cb3c382cf8b24edaa41c9ce8ac790b37b51c354b08f727a1d72845a044a085234e77470c883783'
